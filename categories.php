bbc477bbdcae2d639e9a8258966be852765dafa2
jdofat <143310236+jdofat@users.noreply.github.com>
2025-03-26 14:17:17 -0400

    Update and rename categories.php to categories

--- categories.php
+++
@@ -1,4 +0,0 @@
-INSERT INTO categories (category, category_id) VALUES ('Inspirational', 1);
-INSERT INTO categories (category, category_id) VALUES ('Love', 2);
-INSERT INTO categories (category, category_id) VALUES ('Pain', 3);
-INSERT INTO categories (category, category_id) VALUES ('Education', 4);
