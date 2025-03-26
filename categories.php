24d31b826faccfb69b9d3c58225fab8c5eed0f81
jdofat <143310236+jdofat@users.noreply.github.com>
2025-03-26 14:10:08 -0400

    Create categories.php

---
+++ categories.php
@@ -0,0 +1,4 @@
+INSERT INTO categories (category, category_id) VALUES ('Inspirational', 1);
+INSERT INTO categories (category, category_id) VALUES ('Love', 2);
+INSERT INTO categories (category, category_id) VALUES ('Pain', 3);
+INSERT INTO categories (category, category_id) VALUES ('Education', 4);
