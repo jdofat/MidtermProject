84d8fb7fa0816ba373277ff15ee141b5e1eca104
jdofat <143310236+jdofat@users.noreply.github.com>
2025-03-26 12:31:30 -0400

    Update quotes.php

--- quotes.php
+++ quotes.php
@@ -1,1 +1,13 @@
+/*
+QUOTES: 
 
+ ALL quotes are required to have ALL 3 of the following:
+a) Quote (the quotation itself)
+b) Author
+c) Category
+
+1. A quote with an id = 10.
+
+4. Your author with the id = 5 should have two quotes in the category id = 4. 
+
+*/
