90a23ab4ddab69f419d2831e2dd0f51d77b00124
jdofat <143310236+jdofat@users.noreply.github.com>
2025-03-26 19:46:14 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,117 +1,128 @@
 <?php
 
-// GET
-// categories/ All categories with their ids (id, category)
-// categories/?id=7 The specific category with its id
-// If no categories found for routes above { message: ‘category_id Not Found’ }
+include_once "api/config/database.php";
+include_once "api/models/authors.php";
 
-include_once 'api/config/database.php';
-include_once 'api/models/categories.php';
+$database = new Database();
+$db = $database->getConnection();
+$author = new Authors($db);
 
-    $database = new Database();
-    $db = $database->getConnection();
-    $category = new Categories($db);
-
-    header('Content-Type: application/json');
-
+header('Content-Type: application/json');
 $method = $_SERVER['REQUEST_METHOD'];
 
-// GET by id
-if ($method == 'GET') {
-    if (isset($_GET['category_id'])) {
-        $getresult = $category->getCategoryById($_GET['category_id']);
-        
-        if ($getresult) {
-            echo json_encode($getresult);
-        } else {
-            echo json_encode(["message" => "none found"]);
-        }
-    } else {
-// GET all
-        $getresult = $category->getAllCategories();
-        
+//GET METHODS:
+
+    if ($method == 'GET') {
+
+//GET BY ID
+    if (isset($_GET['author_id'])) {
+        $getresult = $author->getAuthorById($_GET['author_id']);
+
+    if ($getresult) {
+        echo json_encode($getresult);
+    } 
+        else {
+            echo json_encode(['message' => "author_id not found"]);
+    }
+}
+
+//GET ALL
+
+    elseif (empty($_GET['author_id'])) {
+        $getresult = $author->getAllAuthors();
+    
         if (!empty($getresult)) {
             echo json_encode($getresult);
-        } else {
-            echo json_encode(["message" => "none categories found"]);
+        }   
+        else {
+            echo json_encode(['message' => "author_id not found"]);
         }
     }
 }
 
-// POST
-// /categories/ created category (id, category)
-// Note: To create a category, the POST submission MUST contain the category.
-// category_id does not exist { message: ‘category_id Not Found’ }
-// If missing any parameters { message: ‘Missing Required Parameters’ }
 
-    if ($method == 'POST') {
-        $data = json_decode(file_get_contents("php://input"));
+//POST created auth:
 
-    if (empty($data->category)) {
-        echo json_encode(['message' => 'missing parameters']);
-        exit;
-    }
-
-    $category->category = $data->category;
-
-    if ($category->createCategory()) {
-        echo json_encode([
-            'id' => $category->id,
-            'category' => $category->category
-        ]);
-        }
-    else {
-        echo json_encode(['message' => 'category not found']);
-    }
-}
-
-// PUT
-// /categories/ updated category (id, category)
-// Note: To create a category, the PUT submission MUST contain the id and category.
-// category_id does not exist { message: ‘category_id Not Found’ }
-// If missing parameters (except id) { message: ‘Missing Required Parameters’ }
-
-if ($method == 'PUT') {
+if ($method == 'POST') {
     $data = json_decode(file_get_contents("php://input"));
-    
-    if (empty($data->id) || empty($data->category)) {
-        echo json_encode(['message' => 'missing parameters']);
+
+    if (empty($data->author)) {
         exit;
     }
 
-    $category->id = $data->id;
-    $category->category = $data->category;
+    $author->name = $data->author;
 
-    if ($category->updateCategory()) {
+    if ($author->createAuthor()) {
         echo json_encode([
-            'id' => $category->id,
-            'category' => $category->category
+            'id' => $author->id,
+            'author' => $author->name
         ]);
     } else {
-        echo json_encode(['message' => 'category_id not found']);
+        echo json_encode(['message' => 'author_id not found']);
     }
 }
 
-// DELETE
-// /api/categories/ id of deleted category
-// If no quotes found to delete { message: ‘No Quotes Found’ }
-// Note: All delete requests require the id to be submitted.
+//PUT update:
+
+    if ($method == 'PUT') {
+        $data = json_decode(file_get_contents("php://input"));
+
+        if (empty($data->id) || empty($data->author)) {
+            echo json_encode(['message' => 'missing parameters']);
+                exit;
+        }
+
+        if (!$author->getAuthorById($data->id)) {
+            echo json_encode(['message' => "author_id not found"]);
+                exit;
+        }
+
+    $author->id = $data->id;
+    $author->name = $data->author;
+
+        if ($author->updateAuthor()) {
+            echo json_encode([
+                'id' => $author->id,
+                'author' => $author->name
+            ]);
+        }
+        else {
+            echo json_encode(['message' => "missing parameters"]);
+    }
+}
+
+
+//DELETE:
 
     if ($method == 'DELETE') {
         $data = json_decode(file_get_contents("php://input"));
 
     if (empty($data->id)) {
-        exit;
+        echo json_encode(['message' => 'submit id']);
+            exit;
     }
 
-    $category->id = $data->id;
+    if (!$author->getAuthorById($data->id)) {
+        echo json_encode(['message' => 'author_id not found']);
+            exit;
+    }
 
-    if ($category->deleteCategory()) {
-        echo json_encode(['id' => $category->id]);
-    } else {
+    if ($author->deleteAuthor($data->id)) {
+        echo json_encode(['id' => $data->id]);
+    }
+
+    else {
         echo json_encode(['message' => 'none found']);
     }
 }
+
+
+
+
+
+
+
+
 
 
 
